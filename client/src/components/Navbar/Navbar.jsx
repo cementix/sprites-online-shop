3d44821ece4a0edd2986490141e15c80ca6baa0e
cementix <bodakondrackij@gmail.com>
2024-01-06 13:30:23 +0200

    nav lottie anim

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Navbar.module.scss";
 import { Link } from "react-router-dom";
 import {
@@ -7,8 +7,24 @@ import {
   REGISTRATION_ROUTE,
   SHOP_ROUTE,
 } from "../../utils/consts";
+import Lottie from "lottie-react";
+import burger from "../../assets/animations/burger.json";
 
 const Navbar = ({ isVisible, isScrolled }) => {
+  const burgerRef = useRef();
+
+  const [isPlayingForward, setIsPlayingForward] = useState(true);
+
+  const handleBurgerClick = () => {
+    if (isPlayingForward) {
+      burgerRef.current.playSegments([0, 60], true);
+    } else {
+      burgerRef.current.setDirection(-1);
+      burgerRef.current.play();
+    }
+    setIsPlayingForward(!isPlayingForward);
+  };
+
   return (
     <nav
       className={`${styles.navbar} ${isVisible ? styles.visible : ""} ${
@@ -32,6 +48,13 @@ const Navbar = ({ isVisible, isScrolled }) => {
           GET STARTED
         </Link>
       </ul>
+      <Lottie
+        animationData={burger}
+        style={{ width: 100, height: 100, cursor: "pointer" }}
+        lottieRef={burgerRef}
+        loop={false}
+        onClick={handleBurgerClick}
+      />
     </nav>
   );
 };
